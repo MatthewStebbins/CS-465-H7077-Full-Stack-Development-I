0390b8304ccc5603dd0d150b113996d9e1fc9da7
Matthew Stebbins <mattcatt89@yahoo.com>
2023-11-11 13:39:53 -0600

    Module 3 - static to json

--- app.js
+++ app.js
@@ -7,16 +7,15 @@ const hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-const travelRouter = require('./app_server/routes/travel');
-//const { handlebars } = require('hbs');
+var travelRouter = require('./app_server/routes/travel');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// register handlebars partials(https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
 
 app.set('view engine', 'hbs');
 
@@ -29,6 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
