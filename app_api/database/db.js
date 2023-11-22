4e8773dc5a52ead91b04f7a31d3bfe9cfbab9264
Matthew Stebbins <mattcatt89@yahoo.com>
2023-11-21 19:16:06 -0600

    Module 5 - adding a DB

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,5 +1,5 @@
-const { default: mongoose } = require('mongoose');
-const mangoose = require('mongoose');
+//const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
 const dbURL = `mongodb://${host}/travlr`;
 const readLine = require('readline');
